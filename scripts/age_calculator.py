45f7e27f04b8a8bed4dd4c75086aba962253e682
Joel M <jmasselink@gmail.com>
2018-08-21 23:18:39 -0700

    updated age calculator

---
+++ scripts/age_calculator.py
@@ -0,0 +1,13 @@
+import os, sys
+import pandas as pd, numpy as np
+import datetime as dt
+
+
+# Create Series of names
+
+family = pd.Series('Joel', 'Carol', 'Dad', 'Mom')
+data = []
+
+# enter birthdates for the following people  
+
+pd.DataFrame(data=data, index=family)
